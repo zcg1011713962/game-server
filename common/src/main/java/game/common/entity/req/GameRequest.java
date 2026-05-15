d45fbb6726a23097c2d0c38ce44647a8e45d899d
zcg <zhengchunguang66@gmail.com>
2026-05-15 10:18:09 +0800

    [ADD]

--- common/src/main/java/game/common/entity/req/GameRequest.java
+++ common/src/main/java/game/common/entity/req/GameRequest.java
@@ -2,11 +2,15 @@ package game.common.entity.req;
 
 import com.alibaba.fastjson2.JSONObject;
 import game.common.protocol.Cmd;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GameRequest {
     private String traceId;
     private String gatewayId;
